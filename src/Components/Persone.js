379c33f3ec874068a189ea8acf30e0fc3bdab505
EgorEast <egoreast.true@gmail.com>
2021-05-14 14:22:09 +0300

    renamrd variables and added removeEntry function in Persone.js

--- src/Components/Persone.js
+++ src/Components/Persone.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import TableContext from '../tableContext';
 
-function Persone({ id = -1, firstName = '', lastName = '', age = -1 }) {
-	return [
+function Persone({
+	id = -1,
+	firstName: firstname = '',
+	lastName: lastname = '',
+	age = -1,
+}) {
+	let { removeEntry } = useContext(TableContext);
+	return (
 		<tr>
 			<td>{id}</td>
-			<td>{firstName}</td>
-			<td>{lastName}</td>
+			<td>{firstname}</td>
+			<td>{lastname}</td>
 			<td>{age}</td>
-			<button>&times;</button>
-		</tr>,
-		,
-	];
+			<td className='td-delete-button'>
+				<button
+					className='delete-button'
+					onClick={() => {
+						removeEntry(id);
+					}}
+				>
+					&#10006;
+				</button>
+			</td>
+		</tr>
+	);
 }
 export default Persone;
