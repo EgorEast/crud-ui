8669b097f2bb99c5292b43185715519979c2fbba
EgorEast <egoreast.true@gmail.com>
2021-05-14 13:22:50 +0300

    delete Component hook in the App.js and AddEntry.ls

--- src/Components/AddEntry.js
+++ src/Components/AddEntry.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 
 function AddEntry({ onCreate = () => {} }) {
 	let [firstName, setFirstName] = useState('');
