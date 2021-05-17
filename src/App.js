796591ada5af0aa6bf79d718b8a2da2e4a8ec02d
EgorEast <egoreast.true@gmail.com>
2021-05-17 12:06:14 +0300

    reception of data from the server

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from './Components/Table';
 import './App.css';
 import TableContext from './tableContext';
-import data from './Components/data.json';
 import AddEntry from './Components/AddEntry';
 
 function App() {
-	let [listOfPeople, setPersone] = useState(data.listOfPeople);
+	let [error, setError] = useState(null);
+	let [isLoaded, setLoaded] = useState(false);
+	let [peopleList, setPeopleList] = useState([]);
 
-	function addEntry(firstName, lastName, age) {
-		setPersone(
-			listOfPeople.concat([
-				{ firstName, lastName, age, id: listOfPeople.length + 1 },
+	useEffect(() => {
+		fetch('http://178.128.196.163:3000/api/records')
+			.then((res) => res.json())
+			.then(
+				(result) => {
+					setLoaded(true);
+					setPeopleList(result);
+				},
+				(error) => {
+					setLoaded(true);
+					setError(error);
+				}
+			);
+	}, []);
+
+	peopleList.map((persone, index) => {
+		persone.index = index;
+	});
+
+	function addEntry(name, age, _id, __v) {
+		setPeopleList(
+			peopleList.concat([
+				{ _id, data: { name, age }, __v, index: peopleList.length + 1 },
 			])
 		);
 	}
 
-	function removeEntry(id) {
-		setPersone(listOfPeople.filter((persone) => persone.id !== id));
+	function removeEntry(index, id) {
+		// if (peopleList[index].__v == 0) {
+		// 	console.log('Не надо удалять');
+		// } else {
+		// 	setPeopleList(peopleList.filter((persone) => persone._id !== id));
+		// }
 	}
 
-	return (
-		<TableContext.Provider value={{ listOfPeople, addEntry, removeEntry }}>
-			<AddEntry key='add-entry' />
-			<Table key='table' />
-		</TableContext.Provider>
-	);
+	if (error) return <p>Error {error.message}</p>;
+	else if (!isLoaded) return <p>Loading...</p>;
+	else
+		return (
+			<TableContext.Provider value={{ peopleList, addEntry, removeEntry }}>
+				<AddEntry key='add-entry' />
+				<Table key='table' />
+			</TableContext.Provider>
+		);
 }
 
 export default App;
