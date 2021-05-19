781f53ae702b9829d66b8913b4e498736c03e411
EgorEast <egoreast.true@gmail.com>
2021-05-19 14:44:51 +0300

    fields of the Persone are replaced to input

--- src/Components/Persone.js
+++ src/Components/Persone.js
@@ -1,21 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import TableContext from '../tableContext';
 
-function Persone({ id = -1, name = '', age = -1 }) {
+function Persone(persone) {
+	let [name, setName] = useState(persone.name);
+	let [age, setAge] = useState(persone.age);
+
 	let { removeEntry } = useContext(TableContext);
+
 	return [
-		<td key={`name-${id}`}>{name}</td>,
-		<td key={`ade-${id}`}>{age}</td>,
-		<td key={`id-${id}`}>{id}</td>,
-		<td className='td-delete-button' key={`td-delete-button-${id}`}>
+		<td key={`name-${persone.id}`}>
+			<input
+				key={`name-input-${persone.id}`}
+				type='text'
+				value={name}
+				onChange={(event) => {
+					setName(event.target.value);
+				}}
+				disabled={true}
+			/>
+		</td>,
+		<td key={`ade-${persone.id}`}>
+			<input
+				key={`age-input-${persone.id}`}
+				type='text'
+				value={age}
+				onChange={(event) => {
+					setAge(event.target.value);
+				}}
+				disabled={true}
+			/>
+		</td>,
+		<td key={`id-${persone.id}`}>{persone.id}</td>,
+		<td className='td-delete-button' key={`td-delete-button-${persone.id}`}>
 			<button
+				key={`remove-button-${persone.id}`}
 				className='delete-button'
 				onClick={() => {
-					removeEntry(id);
+					removeEntry(persone.id);
 				}}
 			>
 				&#10006;
 			</button>
+		</td>,
+		<td>
+			<button key={`change-button-${persone.id}`}>Редактировать</button>
 		</td>,
 	];
 }
