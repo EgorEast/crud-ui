0472681755cde03c744e75229f6c9af34acdbcbf
EgorEast <egoreast.true@gmail.com>
2021-05-19 14:42:28 +0300

    set random id

--- src/Components/AddEntry.js
+++ src/Components/AddEntry.js
@@ -1,63 +1,78 @@
 import React, { useContext, useState } from 'react';
 import TableContext from '../tableContext';
 
+function randInt(min, max) {
+	// случайное число от min до (max+1)
+	let rand = min + Math.random() * (max + 1 - min);
+	return Math.floor(rand);
+}
+
+let newID = (idLenght, peopleList) => {
+	let isCorrectId = true;
+	let id = '';
+	const letters = ['a', 'b', 'c', 'd', 'e', 'f'];
+	do {
+		while (id.length < idLenght) {
+			const rand = randInt(0, 15);
+			id += rand < 10 ? rand : letters[rand - 10];
+		}
+		isCorrectId = true;
+		peopleList.forEach((persone) => {
+			if (id === persone._id) {
+				isCorrectId = false;
+				console.log('ne poluchaetsa');
+				return;
+			}
+		});
+	} while (!isCorrectId);
+	return id;
+};
+
 function AddEntry() {
-	let { addEntry } = useContext(TableContext);
+	let { addEntry, peopleList } = useContext(TableContext);
 
 	let [name, setName] = useState('');
 	let [age, setAge] = useState('');
-	let [id, setId] = useState('');
+	let id;
 
 	function submitHandler(event) {
 		event.preventDefault();
 
-		if (name && id && age) {
+		id = newID(24, peopleList);
+
+		if (name && age) {
 			addEntry(name, age, id, 0);
 			setName('');
-			setId('');
+			id = 0;
 			setAge('');
 		} else console.log('Заполните все поля');
 	}
 
 	return (
 		<form className='add-form' onSubmit={submitHandler}>
-			<div>
-				<input
-					type='text'
-					className='input'
-					value={name}
-					name='firstName'
-					onChange={(event) => {
-						setName(event.target.value);
-					}}
-					placeholder='Name'
-				></input>
-				<input
-					type='number'
-					className='input'
-					value={age}
-					name='age'
-					onChange={(event) => {
-						setAge(event.target.value);
-					}}
-					placeholder='Age'
-				></input>
-				<input
-					type='text'
-					className='input'
-					value={id}
-					name='ID'
-					onChange={(event) => {
-						setId(event.target.value);
-					}}
-					placeholder='ID'
-				></input>
-			</div>
-			<div>
-				<button className='add-btn' type='submit'>
-					add
-				</button>
-			</div>
+			<input
+				type='text'
+				className='add-form__input'
+				value={name}
+				name='firstName'
+				onChange={(event) => {
+					setName(event.target.value);
+				}}
+				placeholder='Name'
+			></input>
+			<input
+				type='number'
+				className='add-form__input'
+				value={age}
+				name='age'
+				onChange={(event) => {
+					setAge(event.target.value);
+				}}
+				placeholder='Age'
+			></input>
+			<button className='add-btn' type='submit'>
+				add
+			</button>
 		</form>
 	);
 }
