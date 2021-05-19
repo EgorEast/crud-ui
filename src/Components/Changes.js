4fcc5c58cbc439c8fcd04cc3336bb1e9d8a510e6
EgorEast <egoreast.true@gmail.com>
2021-05-19 16:33:38 +0300

    The button to accept changes works

--- src/Components/Changes.js
+++ src/Components/Changes.js
@@ -2,10 +2,10 @@ import React, { useContext } from 'react';
 import TableContext from '../tableContext';
 
 function Changes() {
+	let { acceptChanges } = useContext(TableContext);
 	return (
 		<div className='chandes-container'>
-			<button>Send Changes</button>
-			<button>Accept Changes</button>
+			<button onClick={acceptChanges()}>Accept Changes</button>
 		</div>
 	);
 }
